1d896bffc490fbe0b42a1b1ed60360e6dd7e7ed1
Umer_237 <umerafzal981@gmail.com>
2023-05-22 15:43:20 +0500

    umer

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import Banner from './Banner'
 import OurServices from './OurServices'
 import Chooseus from './Chooseus'
 import OurTeam from './OurTeam'
+import Footer from './Footer'
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
     <OurServices/>
     <Chooseus/>
     <OurTeam/>
+    <Footer/>
     </>
   )
 }
